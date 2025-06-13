be51d00144b50b9a8ea06cefb0fc59da9e15f6aa
henryaudi <henryperfectest@gmail.com>
2025-06-13 08:59:38 -0600

    New commit from 06/12/25 leftovers

--- mycoolapp/src/main/java/com/shangjiezheng/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/shangjiezheng/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,25 @@
 package com.shangjiezheng.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "teamInfo".
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team Name: " + teamName;
+    }
 
     // Expose "/" endpoint that returns "Hello World".
     @GetMapping("/")
@@ -23,4 +38,6 @@ public class FunRestController {
     public String getFortune() {
         return "Today is your lucky day!";
     }
+
+
 }
