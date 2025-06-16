e46b1d4bb87f3315aecde1196e9e02a6a4814216
henryaudi <henryperfectest@gmail.com>
2025-06-15 23:54:35 -0600

    Add qualifier

--- springcoredemo/src/main/java/com/shangjie/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/shangjie/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.shangjie.springcoredemo.rest;
 
 import com.shangjie.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class DemoController {
 
     // Define a constructor for dependency injection
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("baseballCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 
