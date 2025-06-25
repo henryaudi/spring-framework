d858edf16743930b324a32b67af4756edc00c044
henryaudi <henryperfectest@gmail.com>
2025-06-25 11:33:00 -0600

    Add DAO method and impl to Create new Student objects

--- cruddemo/src/main/java/com/shangjie/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/shangjie/cruddemo/CruddemoApplication.java
@@ -1,7 +1,11 @@
 package com.shangjie.cruddemo;
 
+import com.shangjie.cruddemo.dao.StudentDAO;
+import com.shangjie.cruddemo.entity.Student;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -10,4 +14,40 @@ public class CruddemoApplication {
         SpringApplication.run(CruddemoApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+
+        return args -> {
+            createMultipleStudents(studentDAO);
+        };
+    }
+
+    private void createMultipleStudents(StudentDAO studentDAO) {
+
+        // Create multiple students
+        System.out.println("Creating new student objects...");
+        Student tempStudent1 = new Student("John", "Doe", "jdoe@smu.edu");
+        Student tempStudent2 = new Student("Ronald", "Lucas", "rlucas@smu.edu");
+        Student tempStudent3 = new Student("Jane", "Mitt", "jmitt@smu.edu");
+
+        // save the student objects
+        System.out.println("Saving the students...");
+        studentDAO.save(tempStudent1);
+        studentDAO.save(tempStudent2);
+        studentDAO.save(tempStudent3);
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+
+        // create the student object
+        System.out.println("Creating new student object...");
+        Student tempStudent = new Student("Paul", "Doe", "pdoe@smu.edu");
+
+        // save the student object
+        System.out.println("Saving the student...");
+        studentDAO.save(tempStudent);
+
+        // display the id of the saved student
+        System.out.println("Saved student: Generated id: " + tempStudent.getId());
+    }
 }
---
+++ cruddemo/src/main/java/com/shangjie/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.shangjie.cruddemo.dao;
+
+import com.shangjie.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+}
