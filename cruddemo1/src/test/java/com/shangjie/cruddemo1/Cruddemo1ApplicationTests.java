95b22ffd72793eee07c3f5847b9525c24ffd39e1
henryaudi <henryperfectest@gmail.com>
2025-07-11 08:42:02 -0600

    Create project

---
+++ cruddemo1/src/test/java/com/shangjie/cruddemo1/Cruddemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shangjie.cruddemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cruddemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
