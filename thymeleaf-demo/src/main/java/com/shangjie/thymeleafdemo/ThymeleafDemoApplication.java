06556b7e3b0c437dd87c286b88b2e90f7e307dac
henryaudi <henryperfectest@gmail.com>
2025-07-29 08:44:29 -0600

    add more

---
+++ thymeleaf-demo/src/main/java/com/shangjie/thymeleafdemo/ThymeleafDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shangjie.thymeleafdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafDemoApplication.class, args);
+    }
+
+}
